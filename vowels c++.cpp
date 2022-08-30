2bc47647bacd1afc3e14b1cc4125dc4eaa603a98
B.sujith <110800611+sujith4203@users.noreply.github.com>
2022-08-30 12:16:07 +0530

    Add files via upload

---
+++ vowels c++.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+// main function
+int main()
+{
+    char c;
+
+    c='U';
+        
+    //checking for vowels	
+    if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+    c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+    {
+        cout << c << " is a vowel";  //condition true input is vowel
+    }
+    else
+    {
+        cout << c << " is a consonant";  //condition false input is consonant
+    }
+
+    return 0;
+}
